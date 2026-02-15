14a702b118045522048a9144635330ee69bf6cce
Richmond <gonzalesrichmond321@gmail.com>
2026-02-16 07:31:47 +0800

    Add Transaction UI changes

--- app/src/main/java/com/waray/spendhound/AddTransactionActivity.java
+++ app/src/main/java/com/waray/spendhound/AddTransactionActivity.java
@@ -64,7 +64,7 @@ public class AddTransactionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_transcation);
+        setContentView(R.layout.activity_add_transaction);
 
         // Get the Firebase Authentication instance
         mAuth = DeclareDatabase.getAuth();
